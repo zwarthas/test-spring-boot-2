71b6c39adc25fb03372d84fdad79bfbbcffdf4e1
zw <375052868@qq.com>
2020-02-29 21:55:17 +0800

    v0.2

--- src/test/java/com/zw/test/UserTest.java
+++ src/test/java/com/zw/test/UserTest.java
@@ -1,12 +1,7 @@
 package com.zw.test;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import com.zw.App;
-
-
 public class UserTest {
-
+	public static void mian(String[] args) {
+		System.out.println("zw 分支");
+	}
 }
