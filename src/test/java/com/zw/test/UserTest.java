9033b5ba7d182cdced9925ec041ea6452aa0a362
zw <375052868@qq.com>
2020-02-29 21:56:29 +0800

    v0.3

--- src/test/java/com/zw/test/UserTest.java
+++ src/test/java/com/zw/test/UserTest.java
@@ -1,11 +1,5 @@
 package com.zw.test;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import com.zw.App;
-
 
 public class UserTest {
 
